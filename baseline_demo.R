8bf988e5bedb00ee1396202d8b71c72e31d643f9
Sam Rippley <samrippley@Sams-MacBook-Air-2.local>
2022-06-08 16:14:35 -0400

    saved file

--- baseline_demo.R
+++ baseline_demo.R
@@ -1,9 +1,10 @@
+library("dplyr")
 
-library(dplyr)
+load("~/Virginia Tech/Internship 2022/2022-DSPG-LivDiv-/data/livdivdata.RData")
 
 baseline <- livdiv %>%
   filter(week_num == 0:4) %>%
-  arrange(date) %>%
+  arrange(date)
 
 baseline %>%
   summarise("farm_yn")
@@ -15,9 +16,12 @@ sumstats <- baseline %>%
 summarise_at(c("farm_yn", "head_edu", "head_age", "head_married", "head_female", "nb_children", "hh_dependents_prop"), mean, na.rm = TRUE)
 
 sumstats2 <- baseline %>%
-  summarise_at(c("farm_yn", "head_edu", "head_age", "head_married", "head_female", "nb_children", "hh_dependents_prop"), range, na.rm = TRUE)
+  summarise_at(c("farm_yn", "head_edu", "head_age", "head_married", "head_female", "nb_children", "hh_dependents_prop"), min, na.rm = TRUE)
 
-sumstats <- rbind(sumstats, sumstats2)
+sumstats3 <- baseline %>%
+  summarise_at(c("farm_yn", "head_edu", "head_age", "head_married", "head_female", "nb_children", "hh_dependents_prop"), max, na.rm = TRUE)
+
+sumstats <- rbind(sumstats, sumstats2, sumstats3)
 
 rownames <- c("Mean", "Min", "Max")
 row.names(sumstats) <- rownames
@@ -25,12 +29,36 @@ row.names(sumstats) <- rownames
 villagecount <- baseline %>% 
   count(village) 
 
-baseline %>% 
+groupvillage <- baseline %>% 
   group_by(village) %>% 
-  summarize(meanage = mean(head_age), )
+  summarize(meanage = mean(head_age), meanedu = mean(head_edu), meanmarried = mean(head_married), meanfem = mean(head_female))
 
+countfem <- baseline %>%
+  group_by(village) %>%
+  count(head_female) 
   
+countmar <- baseline %>%
+  group_by(village) %>%
+  count(head_married) 
 
+
+
+baseline %>%
+  group_by(head_female) %>%
+  count(village) 
+
+baseline %>%
+  group_by(head_married) %>%
+  count(village) 
+
+groupmar <- baseline %>% 
+  group_by(head_married) %>% 
+  summarize(meanage = mean(head_age), meanedu = mean(head_edu), meanfem = mean(head_female))
+
+
+groupfem <- baseline %>% 
+  group_by(head_female) %>% 
+  summarize(meanage = mean(head_age), meanedu = mean(head_edu), meanmar = mean(head_married))
 
 
          
