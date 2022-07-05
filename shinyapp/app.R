b8f4038ddb9db27866a9aa1c767366d53d060862
Taj Cole <tajc@vt.edu>
2022-07-05 15:06:53 -0400

    commit 7/5

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1158,7 +1158,7 @@ server <- function(input, output, session) {
                                , y = mean_rmt_per_week, color = Villages)) + 
       geom_line() +
       theme_classic() +
-      labs(x = "Date", y = "Average Remittance Income (INR)") +
+      labs(x = "Date", y = "Average Remittance Income (INR)", caption = "Mean: 2622.5  Median: 1731.66") +
       ggtitle("Average Weekly Remittance Income")+ #(11/16/18 - 10/31/19)
       #scale_color_brewer(palette = "Spectral")+
       scale_x_discrete(breaks = c(10,20,30,40), labels = c("January 2019", "April 2019", "July 2019", "October 2019"), limits = 10:40)
