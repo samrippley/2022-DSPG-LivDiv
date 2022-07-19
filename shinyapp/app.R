92d8434f629032a3f704efc1be465ce619b88763
Taj Cole <tajc@vt.edu>
2022-07-19 15:13:11 -0400

    timelapse changes

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -957,14 +957,18 @@ ui <- navbarPage(title = "",
                                               
                                      ),
                             tabPanel("Timelapse", 
-                                     fluidRow(style = "margin: 6px;", align = "justify",
+                                     fluidRow(style = "margin: 6px;",
+                                              align = "center",
                                               p("", style = "padding-top:10px;"),
-                                              column(12, align = "center", h2(strong("Coastal Degradation Timelapse of Sundarbans Area")),
-                                                     p("The video below shows the coastline of the Sundarbans from 1984 to 2022. This timelapse shows that the coastline has degraded significantly over the years. One such factor of this degradation are the effects caused by climate change;
+                                              column(12, 
+                                                     h2(strong("Coastal Degradation Timelapse of Sundarbans Area"), align = "center"),
+                                                     p("The video below shows the coastline of the Sundarbans from 1984 to 2022. This timelapse shows that the coastline has degraded significantly over the years. The circles indicate where this degredation is most evident; some islands have almost completely disappeared. 
+                                                     One such factor of this degradation are the effects caused by climate change;
                                                        one of these effects being the rising of the sea level, resulting in an increase of runoff and the erosion of the coast. This coastal erosion reduces the sediment in the area that acts as a natural buffer to flooding, as well as
                                                        increasing the salinity of groundwater, pushing salt water up stream, ultimately causing a decrease in the supply of drinkable water.
-                                                       The thinning of the Sundarbans coast also negatively impacts households’ agricultural yields, which families heavily depend on to support their livelihoods."),
-                                                     br(""), tags$video(type = "video/mp4",src = "Sundarbansv3 ‑ Made with FlexClip.mp4", width = "60%", align = "center",controls = "controls", autoplay = T, loop = T)
+                                                       The thinning of the Sundarbans coast also negatively impacts households’ agricultural yields. Families heavily depend on these yields to support their livelihoods, as it serves as an
+                                                       essential source of income and food. Due to this, we've observed fluctuations in income, and frequent occurences of households having to reduce or skip meals.", align = "justify"),
+                                                     br(""), tags$video(type = "video/mp4",src = "Sundarbansv3 ‑ Made with FlexClip.mp4", width = "60%", align = "right", controls = "controls", autoplay = T, loop = T)
                                               ), 
                                      )
                                      
