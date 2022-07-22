c78593cc8f829950f5ff8cdd2182209681e94842
Taj Cole <tajc@vt.edu>
2022-07-22 15:31:46 -0400

    chnaged events

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -900,7 +900,7 @@ filtered_non_food_cs <- reactive({
 })
 
 # Events data -------------------------------------
-Events <- c("Khairf Crop Preparation","Kharif Crop Harvest", "Rabi Crop Harvest","Honey Harvest", "Fani Cyclone", "Matmo/Bulbul Cyclone", "Vayu Cyclone", "Hikaa Cyclone","Kyaar Cyclone","Maha Cyclone",
+Events <- c("Kharif Crop Preparation","Kharif Crop Harvest", "Rabi Crop Harvest","Honey Harvest", "Fani Cyclone", "Matmo/Bulbul Cyclone", "Vayu Cyclone", "Hikaa Cyclone","Kyaar Cyclone","Maha Cyclone",
             "Republic Day", "Rama Navami", "Eid al-Fitr", "Indian Independence Day", "Dussehra", "Diwali" ,"Christmas")
 start_week <- c(28, 2, 0, 19, 22, 48, 30, 43, 47, 48, 10, 20, 28, 38, 46, 49, 5)
 end_week <- c(36, 12, 14, 32, 24, 49, 31, 44, 49, 49, 10.2, 20.2, 28.2, 38.2, 46.2, 49.2, 5.2)
@@ -1384,7 +1384,7 @@ ui <- navbarPage(title = "",
                                          pickerInput("village_exp", "Select Village:", choices = village_vector, 
                                                      selected = village_vector,
                                                      multiple = T, options = list(`actions-box` = T)),
-                                         pickerInput("event_choose_exp", "Select Event:", choices = events_vector, selected = "Kharif Crop Harvest", 
+                                         pickerInput("event_choose_exp", "Select Event:", choices = events_vector, selected = "Kharif Crop Preparation", 
                                                      multiple = T, options = list(`actions-box` = T)),
                                          
                                          
@@ -1425,7 +1425,7 @@ ui <- navbarPage(title = "",
                                          pickerInput("village_cs", "Select Village:", choices = village_vector,
                                                      selected = village_vector,
                                                      multiple = T, options = list(`actions-box` = T)),
-                                         pickerInput("event_choose_cs", "Select Event:", choices = events_vector, selected = "Kharif Crop Harvest", 
+                                         pickerInput("event_choose_cs", "Select Event:", choices = events_vector, selected = "Kharif Crop Preparation", 
                                                      multiple = T, options = list(`actions-box` = T)),
                                          
                                        ),
@@ -1488,7 +1488,7 @@ ui <- navbarPage(title = "",
                                                      selected = village_vector,
                                                      multiple = T, options = list(`actions-box` = T)),
                                          varSelectInput("nonfood_group", "Select Consumption Group:", non_food_cs[,-(1:2)]),
-                                         pickerInput("event_choose_cs_nonfood", "Select Event:", choices = events_vector, selected = "Kharif Crop Harvest", 
+                                         pickerInput("event_choose_cs_nonfood", "Select Event:", choices = events_vector, selected = "Kharif Crop Preparation", 
                                                      multiple = T, options = list(`actions-box` = T)),
                                          
                                        ),
@@ -1539,7 +1539,7 @@ ui <- navbarPage(title = "",
                                                      selected = village_vector,
                                                      multiple = T, options = list(`actions-box` = T)),
                                          varSelectInput("Gender", "Select Gender:", malefemale_inc[,-(1:2)]),
-                                         pickerInput("event_choose_inc", "Select Event:", choices = events_vector, selected = "Kharif Crop Harvest", 
+                                         pickerInput("event_choose_inc", "Select Event:", choices = events_vector, selected = "Kharif Crop Preparation", 
                                                      multiple = T, options = list(`actions-box` = T)),
                                        ),
                                        # Show a plot of the generated plot
@@ -1569,7 +1569,7 @@ ui <- navbarPage(title = "",
                                        sidebarPanel(pickerInput("village_bramt", "Select Village:", choices = village_vector, 
                                                                 selected = village_vector, 
                                                                 multiple = T, options = list(`actions-box` = T)),
-                                                    pickerInput("event_choose_borr", "Select Event:", choices = events_vector, selected = "Kharif Crop Harvest", 
+                                                    pickerInput("event_choose_borr", "Select Event:", choices = events_vector, selected = "Kharif Crop Preparation", 
                                                                 multiple = T, options = list(`actions-box` = T))
                                        ),
                                      
@@ -1664,7 +1664,7 @@ ui <- navbarPage(title = "",
                                          pickerInput("village_rmt", "Select Village:", choices = village_vector, 
                                                      selected = village_vector,
                                                      multiple = T, options = list(`actions-box` = T)),
-                                         pickerInput("event_choose_rmt", "Select Event:", choices = events_vector, selected = "Kharif Crop Harvest", 
+                                         pickerInput("event_choose_rmt", "Select Event:", choices = events_vector, selected = "Kharif Crop Preparation", 
                                                      multiple = T, options = list(`actions-box` = T)),
                                          
                                        ),
